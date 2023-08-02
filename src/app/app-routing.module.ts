6bf0e769de5a6446a34ecf01a65935e2695f2ae1
Pierre Juarez <76144339+pierre-juarez@users.noreply.github.com>
2023-08-02 12:41:38 -0500

    [add]: Configurations

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GameListComponent } from './components/game-list/game-list.component';
+import { GameDetailComponent } from './components/game-detail/game-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: GameListComponent }, 
+  { path: 'game/:id', component: GameDetailComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' } // En caso de no encontrar una ruta válida
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GameListComponent } from './components/game-list/game-list.component';
+import { GameDetailComponent } from './components/game-detail/game-detail.component';
+import { HttpClientModule } from '@angular/common/http';
+import { GameServiceService } from './services/game-service.service';
+import { GameNavbarComponent } from './components/game-navbar/game-navbar.component';
+import { GameFooterComponent } from './components/game-footer/game-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameListComponent,
+    GameDetailComponent,
+    GameNavbarComponent,
+    GameFooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+     HttpClientModule
   ],
-  providers: [],
+  providers: [GameServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
