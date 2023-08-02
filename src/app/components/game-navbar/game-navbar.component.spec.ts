c17f4516bb07de31feeeb9c4184b9ac86c2f4a92
Pierre Juarez <76144339+pierre-juarez@users.noreply.github.com>
2023-08-02 16:52:34 -0500

    [add] - Unit Tets Navbar

--- src/app/components/game-navbar/game-navbar.component.spec.ts
+++ src/app/components/game-navbar/game-navbar.component.spec.ts
@@ -1,21 +1,36 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { GameNavbarComponent } from './game-navbar.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
 
 describe('GameNavbarComponent', () => {
   let component: GameNavbarComponent;
   let fixture: ComponentFixture<GameNavbarComponent>;
+  let router: Router;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [GameNavbarComponent],
+      imports: [RouterTestingModule]
+    }).compileComponents();
+  });
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [GameNavbarComponent]
-    });
     fixture = TestBed.createComponent(GameNavbarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    router = TestBed.inject(Router);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should display the welcome user message', () => {
+    component.username = 'Pierre';
+    fixture.detectChanges();
+    const welcomeUserElement = fixture.nativeElement.querySelector('.welcome-user');
+    expect(welcomeUserElement.textContent).toContain('Welcome, Pierre!');
+  });
+
 });
--- src/app/components/game-navbar/game-navbar.component.ts
+++ src/app/components/game-navbar/game-navbar.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class GameNavbarComponent {
 
+  username: string = "Pierre";
+
 }
