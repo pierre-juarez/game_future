b14cd72e944ee70a5b13a25f02fc97c830ace2b8
Pierre Juarez <76144339+pierre-juarez@users.noreply.github.com>
2023-08-03 01:06:31 -0500

    [add]: Unit Test Service & AppComponent

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'listGames'`, () => {
+  it(`should have as title 'GameFuture'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('listGames');
+    expect(app.title).toEqual('GameFuture');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('listGames app is running!');
-  });
+  
 });
--- src/app/services/game-service.service.spec.ts
+++ src/app/services/game-service.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { GameServiceService } from './game-service.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('GameServiceService', () => {
   let service: GameServiceService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule]
+    });
     service = TestBed.inject(GameServiceService);
   });
 
